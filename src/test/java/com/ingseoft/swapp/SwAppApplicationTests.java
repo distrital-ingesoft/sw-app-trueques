b3f0505db69d38445da1b8407422b110c83077a7
Christian Unigarro <cunigarrod@gmail.com>
2022-11-12 09:43:48 -0500

    Initial project.

---
+++ src/test/java/com/ingseoft/swapp/SwAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingseoft.swapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
